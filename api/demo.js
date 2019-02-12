1eb356441105f5429c96ca762b29a6570733fd7d
colin <i@wangyunxiao.com>
2019-02-12 23:39:25 +0800

    init project

---
+++ api/demo.js
@@ -0,0 +1,10 @@
+const query = require('../db');
+async function showData (ctx, next) {
+    let rows = await query('SELECT * FROM menu');
+    ctx.body = {
+        code: 1,
+        data: rows,
+        msg: 'success'
+    }
+}
+module.exports = {showData}
