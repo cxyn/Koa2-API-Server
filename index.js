f2430642a9fc73510ef940a699287ef64542683d
WangYunxiao <wangyx@htd.cn>
2019-02-13 16:42:19 +0800

    add favicon

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const Koa = require('koa')
 const app = new Koa()
 const Router = require('koa-router')
 const router = new Router()
+const favicon = require('koa-favicon')
 require('dotenv').config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3003
 const bodyParser = require('koa-bodyparser')
-const query = require('./db')
+const query = require('./config/db')
 const logger = require('koa-logger')
 const cors = require('koa2-cors')
-console.log()
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger())
 app.use(cors())
 app.use(bodyParser())
@@ -20,4 +22,4 @@ app.use(bodyParser())
 require('./routes')(router)
 
 app.use(router.routes()).use(router.allowedMethods())
-app.listen(port, () => console.log(`API Server is running, the Port: ${port}`))
+app.listen(port, () => console.log(`API Server is running, the listening port is: ${port}`))
